2aed09581529d12e7720a3f19fc3384839f30c31
mjstthomas0516 <mjstthomas0516@gmail.com>
2019-09-13 11:35:47 -0700

    updated rps game

--- script.js
+++ script.js
@@ -1,8 +1,62 @@
-window.setTimeout(function(){
-var playerChoice = prompt("Choose Rock, Paper, or Scissors")
-}, 1000);
-var computerChoices = ["Rock", "Paper", "Scissors"];
 
-function randomChoice(){
+
+
+var round = [1,2,3,4,5];
+var x = 0;
+var y = 0;
+var playWin = "You win!";
+var compWin = "Sorry, you lose!";
+
+
+var compare = function(choice1, choice2){
+	if(choice1 === choice2){
+		i--;
+		return "It is a tie!";
+	}
+	if(choice1 === 'Rock'){
+		if(choice2 === "Paper"){
+			y++;
+			return compWin;
+		}
+		if(choice2 === "Scissors"){
+			x++;
+			return playWin;
+		}	
+	}
+	if(choice1 === 'Paper'){
+		if(choice2 === 'Scissors'){
+			y++;
+			return compWin;
+		}
+		if(choice2 === "Rock"){
+			x++;
+			return playWin;
+		}
+	}
+	if(choice1 === 'Scissors'){
+		if(choice2 === 'Rock'){
+			y++;
+			return compWin;
+		}
+		if(choice2 === 'Paper'){
+			x++;
+			return playWin;
+		}	
+	}
+}
+
+function game(){
+	var playerChoice = prompt("Choose Rock, Paper, or Scissors");
+	var computerChoices = ["Rock", "Paper", "Scissors"];
+	function randomChoice(){
 	return computerChoices[Math.floor(Math.random()*computerChoices.length)];
+};
+	alert(compare(playerChoice, randomChoice()));
+	alert(x+" - "+y);
+}
+
+for(var i = 0; i < 6; i++){
+	if(round[i]){
+		game();
+	}
 }
